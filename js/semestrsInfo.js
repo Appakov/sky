78b9f427d8d9b9b6228f721d2bce22114aee17ce
Appakov <147263500+Appakov@users.noreply.github.com>
2024-04-12 22:16:05 +0600

    tab

--- js/semestrsInfo.js
+++ js/semestrsInfo.js
@@ -1,4 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const tabs = document.querySelectorAll('.group__content-btn');
+    const windows = document.querySelectorAll('.group__window');
+
+    // По умолчанию активируем первый таб и его контент
+    tabs[0].classList.add('active');
+    windows[0].classList.add('active');
+
+    tabs.forEach((tab, index) => {
+        tab.addEventListener('click', () => {
+            // Если таб уже активен, то ничего не делаем
+            if (tab.classList.contains('active')) return;
+
+            // Убираем класс active у всех табов и окон
+            tabs.forEach(t => t.classList.remove('active'));
+            windows.forEach(w => w.classList.remove('active'));
+
+            // Добавляем класс active только к выбранному табу и его окну контента
+            tab.classList.add('active');
+            windows[index].classList.add('active');
+        });
+    });
+});
+document.addEventListener("DOMContentLoaded", () => {
     const buttons = document.querySelectorAll(".btn-learn-info");
     const modals = document.querySelectorAll(".popup");
     const popupClose = document.querySelectorAll('.popup__close-btn')
@@ -14,7 +37,7 @@ document.addEventListener("DOMContentLoaded", () => {
         })
     })
 });
-const testingCompleteBtn = document.querySelector('.testing-complete__btn')
+const testingCompleteBtn = document.querySelector('.testing-completebtn')
 const popupСompleteСloseBtn = document.querySelector('.popup-complete__close-btn')
 const PopupComplete = document.querySelector('.popup-complete')
 
@@ -24,4 +47,3 @@ testingCompleteBtn.onclick = () => {
 popupСompleteСloseBtn.onclick = () => {
     PopupComplete.classList.remove('open')
 }
-
