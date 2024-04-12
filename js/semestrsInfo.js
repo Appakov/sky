27ef5f4172ec2da3eb79a58be6b9278b73f09fd1
Appakov <147263500+Appakov@users.noreply.github.com>
2024-04-12 21:45:10 +0600

    end

--- js/semestrsInfo.js
+++ js/semestrsInfo.js
@@ -21,3 +21,29 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+document.addEventListener("DOMContentLoaded", () => {
+    const buttons = document.querySelectorAll(".btn-learn-info");
+    const modals = document.querySelectorAll(".popup");
+    const popupClose = document.querySelectorAll('.popup__close-btn')
+
+    buttons.forEach((button, index) => {
+        button.addEventListener("click", () => {
+            modals[index].classList.add('open')
+        });
+    });
+    popupClose.forEach((btn, ind) => {
+        btn.addEventListener('click', () => {
+            modals[ind].classList.remove('open')
+        })
+    })
+});
+const testingCompleteBtn = document.querySelector('.testing-completebtn')
+const popupСompleteСloseBtn = document.querySelector('.popup-complete__close-btn')
+const PopupComplete = document.querySelector('.popup-complete')
+
+testingCompleteBtn.onclick = () => {
+    PopupComplete.classList.add('open')
+}
+popupСompleteСloseBtn.onclick = () => {
+    PopupComplete.classList.remove('open')
+}
